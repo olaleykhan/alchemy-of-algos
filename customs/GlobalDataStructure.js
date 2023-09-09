12186b87322b84e3e2796fd0091a3266832188b5
lekan.dev <realjoy9@gmail.com>
2023-09-09 07:07:40 +0100

    custom data structures folder struct

---
+++ customs/GlobalDataStructure.js
@@ -0,0 +1,1 @@
+console.log('global struct looks like this.');
---
+++ linked-lists/linkedList.js
@@ -0,0 +1,5 @@
+console.log("this might be a linked list.");
+console.log("it's a data structure that looks like this:");
+console.log("head -> node -> node -> node -> null");
+console.log("each node has a value and a pointer to the next node.");
+console.log("the last node's pointer is null.");
